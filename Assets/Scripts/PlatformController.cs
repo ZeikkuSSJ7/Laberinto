404623f2a413280ced790ce496ec5f3ce4d200c4
ZeikkuSSJ7 <47304153+ZeikkuSSJ7@users.noreply.github.com>
2021-10-20 10:04:16 +0200

    Ultimo commit

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,11 @@ public class PlatformController : MonoBehaviour
 {
     public float torque;
     private Rigidbody rb;
-
+    private Quaternion rotation;
+    private Quaternion rotMax = Quaternion.Euler(30, 30, 30);
+    private Quaternion rotMin = Quaternion.Euler(-30, -30, -30);
+    private float turnX;
+    private float turnY;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -15,9 +20,42 @@ public class PlatformController : MonoBehaviour
 
     void FixedUpdate()
     {
-        float turnX = Input.GetAxis("Horizontal");
-        float turnY = Input.GetAxis("VerticalJ2");
-        rb.AddTorque(Vector3.forward * torque * -turnX);
-        rb.AddTorque(Vector3.right * torque * -turnY);
+        turnX = Input.GetAxis("Horizontal");
+        turnY = Input.GetAxis("VerticalJ2");
+
+        rotation = transform.rotation;
+        if (canRotateZ())
+            rb.AddTorque(Vector3.forward * torque * -turnX);
+        if (canRotateX())
+            rb.AddTorque(Vector3.right * torque * -turnY);
+        transform.rotation = rotation;
+    }
+
+    private bool canRotateZ()
+    {
+        if (rotation.z < -0.21 && turnX > 0)
+        {
+            return false;
+        }
+        else if (rotation.z > 0.21 && turnX < 0)
+        {
+            return false;
+        }
+        else
+            return true;
+    }
+
+    private bool canRotateX()
+    {
+        if (rotation.x < -0.21 && turnY > 0)
+        {
+            return false;
+        }
+        else if (rotation.x > 0.21 && turnY < 0)
+        {
+            return false;
+        }
+        else
+            return true;
     }
 }
